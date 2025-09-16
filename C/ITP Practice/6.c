66927168d7dc56ccfa8449cdec069805403b5dd9
BhaveshG29072007 <bhaveshpgaikwad@gmail.com>
2025-09-17 00:08:37 +0530

    ITP

---
+++ C/ITP Practice/6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, k;
+    if (scanf("%d %d", &n, &k) != 2) return 0;
+
+    int a[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    if (n > 0) {
+        k %= n;                  
+      
+        for (int l = 0, r = n - 1; l < r; l++, r--) {
+            int t = a[l]; a[l] = a[r]; a[r] = t;
+        }
+        
+        for (int l = 0, r = k - 1; l < r; l++, r--) {
+            int t = a[l]; a[l] = a[r]; a[r] = t;
+        }
+        
+        for (int l = k, r = n - 1; l < r; l++, r--) {
+            int t = a[l]; a[l] = a[r]; a[r] = t;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (i) putchar(' ');
+        printf("%d", a[i]);
+    }
+    putchar('\n');
+    return 0;
+}
+
---
+++ C/ITP Practice/7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    int present[N+1];
+    for (int i = 1; i <= N; i++) {
+        present[i] = 0;
+    }
+
+    for (int i = 0; i < N - 2; i++) {
+        int x;
+        scanf("%d", &x);
+        if (x >= 1 && x <= N) {
+            present[x] = 1;
+        }
+    }
+
+    int missing1 = 0, missing2 = 0;
+    for (int i = 1; i <= N; i++) {
+        if (present[i] == 0) {
+            if (missing1 == 0) {
+                missing1 = i;
+            } else {
+                missing2 = i;
+                break;
+            }
+        }
+    }
+
+    if (missing1 < missing2) {
+        printf("%d %d\n", missing1, missing2);
+    } else {
+        printf("%d %d\n", missing2, missing1);
+    }
+
+    return 0;
+}
+
